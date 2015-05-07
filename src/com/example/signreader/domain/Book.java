8b2bcdf5686140a5c0b8c0f6a851747d709df4c1
ShenDavid <603217995@qq.com>
2015-05-07 22:40:14 +0800

    Update Book.java

--- src/com/example/signreader/domain/Book.java
+++ src/com/example/signreader/domain/Book.java
@@ -3,6 +3,9 @@ package com.example.signreader.domain;
 public class Book {
 	private int id;
 	private String bookPath;
+	private int word;
+	private String name;
+	
 	public int getId() {
 		return id;
 	}
@@ -15,5 +18,36 @@ public class Book {
 	public void setBookPath(String bookPath) {
 		this.bookPath = bookPath;
 	}
+	public int getWord() {
+		return word;
+	}
+	public void setWord(int word) {
+		this.word = word;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", bookPath=" + bookPath + ", word=" + word
+				+ ", name=" + name + "]";
+	}
+	
+	public Book(int id, String bookPath, int word, String name) {
+		super();
+		this.id = id;
+		this.bookPath = bookPath;
+		this.word = word;
+		this.name = name;
+	}
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 }
